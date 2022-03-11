bae432e08fed414b99175bdabf0fffc4f42f89bf
Youngcheor Ra <dksrudmoe@naver.com>
2022-03-11 10:40:25 +0900

    Create 43105.py

---
+++ 동적계획법/43105/43105.py
@@ -0,0 +1,7 @@
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        triangle[i][0] += triangle[i-1][0]
+        for j in range(1, len(triangle[i])-1):
+            triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+        triangle[i][-1] += triangle[i-1][-1]
+    return max(triangle[-1])
